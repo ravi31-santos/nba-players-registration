31c32abbe971f1ccdc76c04c989f7748abe7b831
ravi31-santos <cardosoravi668@gmail.com>
2025-09-05 15:23:28 -0300

    Atualização do projeto

--- rhav/src/main/java/com/project/rhav/Controller/PlayerController.java
+++ rhav/src/main/java/com/project/rhav/Controller/PlayerController.java
@@ -2,41 +2,53 @@ package com.project.rhav.Controller;
 
 import com.project.rhav.Model.Player;
 import com.project.rhav.Repository.PlayerRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
-@RestController
+@Controller
 @RequestMapping("/player")
 public class PlayerController {
 
     private final PlayerRepository repository;
 
     public PlayerController(PlayerRepository repository){
-        this.repository=repository;
-    }
-    @GetMapping
-    public List<Player> listar(){
-        return repository.findAll();
-    }
-    @PostMapping
-    public Player criar(@RequestBody Player player){
-        return repository.save(player);
-    }
-    @PutMapping("/{id}")
-    public Player atualizar(@PathVariable Long id,@RequestBody Player player){
-        Player existente = repository.findById(id).orElseThrow();
-        existente.setNome_jogador(player.getNome_jogador());
-        existente.setNacionalidade_jogador(player.getNacionalidade_jogador());
-        existente.setAltura_jogador(player.getAltura_jogador());
-        existente.setNum_camisa(player.getNum_camisa());
-        existente.setTime_jogador(player.getTime_jogador());
-        existente.setPeso_jogador(player.getPeso_jogador());
-        return repository.save(existente);
-    }
-    @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id){
-        repository.deleteById(id);
+        this.repository = repository;
     }
 
+
+    @GetMapping
+    public String listar(Model model){
+        model.addAttribute("players", repository.findAll());
+        return "players";
+    }
+
+
+    @GetMapping("/new")
+    public String novo(Model model) {
+        model.addAttribute("player", new Player());
+        return "player-form"; // abre player-form.html
+    }
+
+
+    @PostMapping
+    public String salvar(@ModelAttribute Player player){
+        repository.save(player);
+        return "redirect:/player";
+    }
+
+
+    @GetMapping("/edit/{id}")
+    public String editar(@PathVariable Long id, Model model) {
+        Player existente = repository.findById(id).orElseThrow();
+        model.addAttribute("player", existente);
+        return "player-form";
+    }
+
+
+    @GetMapping("/delete/{id}")
+    public String deletar(@PathVariable Long id){
+        repository.deleteById(id);
+        return "redirect:/player";
+    }
 }
